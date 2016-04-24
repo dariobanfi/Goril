ff2ba88287a57777623cd085d5a1f8e629b3f2b4
Dario Banfi <dario.banfi9@gmail.com>
2016-04-24 16:53:47 -0700

    Generalising FallingObject

---
+++ core/src/com/ambigioz/goril/models/objects/Coconut.java
@@ -0,0 +1,85 @@
+package com.ambigioz.goril.models.objects;
+
+import com.ambigioz.goril.util.Assets;
+import com.ambigioz.goril.util.Constants;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+public class Coconut implements FallingObject {
+
+    private float positionX;
+    private float positionY;
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public float getPositionY() {
+        return positionY;
+    }
+
+    public float getSizeX() {
+        return sizeX;
+    }
+
+    public float getSizeY() {
+        return sizeY;
+    }
+
+    private float sizeX;
+    private float sizeY;
+
+    /**
+     *
+     * @param positionX min 0, max 100
+     * @param positionY
+     * @param sizeX
+     * @param sizeY
+     */
+    public Coconut(float positionX, float positionY, float sizeX, float sizeY) {
+        this.positionX = Constants.WIDTH * positionX / 100 / Constants.PPM;
+        if(this.positionX<(sizeX/Constants.PPM/2)){
+            this.positionX = sizeX/Constants.PPM;
+        }
+        else if (this.positionX >= (Constants.WIDTH / Constants.PPM)-sizeX/2/Constants.PPM){
+            this.positionX = (Constants.WIDTH - sizeX)/Constants.PPM;
+        }
+        this.positionY = positionY / Constants.PPM;
+        this.sizeX = sizeX / Constants.PPM;
+        this.sizeY = sizeY / Constants.PPM;
+    }
+
+    public BodyDef getBodyDef(){
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(positionX, positionY);
+        bodyDef.linearDamping = 40.5f;
+        bodyDef.angularDamping = 40.5f;
+
+        return bodyDef;
+    }
+
+
+    public FixtureDef getFixtureDef(){
+        PolygonShape dynamicPolygon = new PolygonShape();
+        dynamicPolygon.setAsBox(sizeX, sizeY);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = dynamicPolygon;
+        fixtureDef.density = 3.1f;
+        fixtureDef.friction = 0.0f;
+        fixtureDef.restitution = 0.0f;
+
+
+        return fixtureDef;
+    }
+
+    public Sprite getSprite(){
+        Sprite polySprite = new Sprite(Assets.diamondRhombus);
+        polySprite.setSize(sizeX * 2, sizeY * 2);
+        polySprite.setOrigin(polySprite.getWidth()  / 2, polySprite.getHeight() / 2);
+        return polySprite;
+    }
+}
